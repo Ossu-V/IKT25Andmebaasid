02992accaea9d42e9ea16585aad2e784e4219dbc
Ossu-V <oscarvalk7@gmail.com>
2026-02-17 14:09:17 +0200

    Create databaseAndTable

---
+++ 1. 17.02.26/IKT25db.sql
@@ -0,0 +1,13 @@
+--teeme andmebaasi ehk db
+create database IKT25tar
+
+--andmebaasi valimine
+use IKT25tar
+
+--andmebaasi kustutamine
+drop database IKT25tar
+
+--teeme uuesti andmebaasi IKT25tar
+create database IKT25tar
+
+--teeme tabeli 
