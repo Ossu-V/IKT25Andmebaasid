50702f8b8e7e4883fab85280bcabd92bd9cf6c3b
Ossu-V <oscarvalk7@gmail.com>
2026-02-17 14:20:23 +0200

    Update IKT25db.sql

--- 1. 17.02.26/IKT25db.sql
+++ 1. 17.02.26/IKT25db.sql
@@ -10,4 +10,44 @@ drop database IKT25tar
 --teeme uuesti andmebaasi IKT25tar
 create database IKT25tar
 
---teeme tabeli 
+--teeme tabeli
+create table Gender
+(
+--Meil on muutuja Id,
+--mis on täisarv andmetüüp,
+--kui sisestad andmed, 
+--siis see veerg peab olema täidetud,
+--tegemist on primaarvõtmega 
+Id int not null primary key,
+--veeru nimi on Gender,
+--10 tähemärki on max pikkus,
+--andmed peavad olema sisestatud ehk
+--ei tohi olla tühi
+Gender nvarchar(10) not null
+)
+
+--andmete sisestamine Gender tabelisse
+-- Id = 1, Gender = Male
+-- Id = 2, Gender = Female
+
+insert into Gender (Id, Gender)
+values (1, 'Male'), 
+       (2, 'Female')
+
+--vaatame tabeli sisu
+-- * tähendab, et näita kõike seal sees olevat infot 
+select * from Gender 
+
+--teeme tabeli nimega Person
+--veeru nimed: Id int not null primary key,
+-- Name nvarchar (30)
+-- Email nvarchar (30)
+-- GenderId int
+
+create table Person
+(
+Id int not null primary key,
+Name nvarchar(30),
+Email nvarchar(30),
+GenderId int
+)
