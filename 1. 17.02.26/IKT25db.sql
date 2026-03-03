1f5e203a98ba258d034870b7bf90931cfa68b4b9
Ossu-V <oscarvalk7@gmail.com>
2026-03-03 14:08:14 +0200

    IKTpe25tar/Andmebaasid

--- 1. 17.02.26/IKT25db.sql
+++ 1. 17.02.26/IKT25db.sql
@@ -278,3 +278,67 @@ from Employees
 left join Department
 on Employees.DepartmentId = Department.Id
 group by Location
+
+select *from Employees
+select sum(cast(Salary as int)) from Employees --arvutab kõikide palgad kokku
+
+--lisame veeru City ja pikkus on 30
+--Employees tabelisse lisada
+
+alter table Employees
+add City nvarchar(30)
+
+select City, Gender, sum(cast(Salary as int)) as TotalSalary 
+from Employees
+group by City, Gender
+
+--peaaegu sama päring, aga linnad on tähestikulises järjestuses
+select City, Gender, sum(cast(Salary as int)) as TotalSalary 
+from Employees
+group by City, Gender
+order by City
+
+--on vaja teada, et mitu inimest on nimekirjas 
+select count(*) from Employees
+
+--mitu töötajat on soo ja linna kaupa töötamas
+select City, Gender, sum(cast(Salary as int)) as TotalSalary, 
+count(Id) as [TotalEmployees(s)]
+from Employees
+group by City, Gender
+order by City
+
+--kuvab kas naised või mehed linnade kaupa
+--kasutage where
+select City, Gender, sum(cast(Salary as int)) as TotalSalary, 
+count(Id) as [TotalEmployees(s)]
+from Employees
+where Gender = 'Male' --saad panna kas Male või Female
+group by Gender, City
+
+--sama tulemus nagu eelmine kord, aga kasutage having
+select City, Gender, sum(cast(Salary as int)) as TotalSalary, 
+count(Id) as [TotalEmployees(s)]
+from Employees
+group by Gender, City
+having Gender = 'Female'
+
+--kõik kes teenivad rohkem, kui 4000
+select * from Employees where sum(cast(Salary as int)) < 4000
+
+--teeme variandi, kus saame tulemuse
+select Gender, City, sum(cast(Salary as int)) as TotalSalary, 
+count(Id) as [TotalEmployees(s)]
+from Employees
+group by Gender, City
+having sum(cast(Salary as int)) > 4000
+
+--loome tabeli, milles hakatakse automaatselt nummerdama Id-d
+create table Test1
+(
+Id int identity(1,1),
+Value nvarchar(20)
+)
+
+insert into Test1 values('X')
+select * from Test1
